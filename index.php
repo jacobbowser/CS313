355599ad8d86f071ec978a24e6375c3618a7ae82
Jacob Bowser <bow13028@byui.edu>
2016-01-16 16:57:09 -0800

    Created the homepage

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <head>
+     <link rel="stylesheet" type="text/css" href="styles.css" />
+     <script type="text/javascript" src="changes.js"></script>
+     <title> Jacob Bowser's Enhanced Home Page </title>
+  </head>
+  <header>
+    <h1 class='header'> 
+        Welcome to Jacob Bowser's </br> Home Page </h1>
+  </header>
+  <body>
+      <a href="assignments.php" >Assignments Page</a>
+    <hr />
+      <i>
+        Jacob Bowser              <br />
+        Computer Science Major    <br />
+        From: Seattle, Washington <br /> <br />
+      </i>
+        <img id='thisImg' onmouseover="newImg()" onmouseout="origImg()" 
+             src = "https://scontent-sea1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-0/p206x206/12039230_10204664131435280_6303937694274217668_n.jpg?oh=4c5193b3e43265b11fff789362bef41c&oe=573D08E2" 
+             alt = "Selfie" height="200" width="200"/>
+      
+      <hr />
+
+        <p class='myIntro'> 
+          Hi, <br /> My name is Jacob Bowser, I am from Seattle, Washington<br /> 
+          I am a computer science major at BYU Idaho. I am on my fifth semester<br /> 
+          I plan to graduate in April of 2017.<br /><br />
+          One of the biggest things happening in my life right now is that I am<br />
+          getting married on February 12th, so it's an exciting time for me right now<br /><br />
+          My number one hobby is playing sports. But my favorite sport is basketball<br />
+          I really have a passion for it, so I like to incorporate it in my<br />
+          programming when I get the chance, hence the basketball pictures and links.<br/>
+        </p>     
+      <a href = "http://en.wikipedia.org/wiki/Michael_Jordan"> 
+        <img id='click' src = "http://lh4.ggpht.com/-6zZDDth1F6Y/UttL-3Z17_I/AAAAAAAAABE/kTTw6m0JGzw/Black-Jordan-Red-Glowing-Logo-psd61307.jpg" 
+             alt = "Michael Jordan" height = "150px" width = "150px"/>
+      </a>
+  </body> 
+</html>
+

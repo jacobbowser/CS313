ac0745eb4f723af2fd84b47e5ba2be30f001a9fe
Jacob Bowser <bow13028@byui.edu>
2016-01-16 17:07:06 -0800

    Added the extra files for the homePage

---
+++ assignments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>  
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+    </head>
+
+    <body>
+        <a href="index.php">Home Page</a>
+        <p>Other Assignments coming soon!</p>
+    </body>
+
+</html>
---
+++ changes.js
@@ -0,0 +1,10 @@
+function newImg() {
+    document.getElementById("thisImg").src = "https://scontent-sea1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/1491767_10208261167993029_2598890592665582535_n.jpg?oh=ba29b38ea937043151e1bc06fcdffa45&oe=56FC9059";
+    document.getElementById("thisImg").width = 300;
+}
+
+function origImg() {
+    document.getElementById("thisImg").src = "https://scontent-sea1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-0/p206x206/12039230_10204664131435280_6303937694274217668_n.jpg?oh=4c5193b3e43265b11fff789362bef41c&oe=573D08E2";
+    document.getElementById("thisImg").width = 200;
+}
+
